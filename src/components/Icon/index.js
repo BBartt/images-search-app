4564461d1fc8840a5351819aa070166440f14078
Bartosz <bartosz.polakowski@escola.pl>
2021-02-07 18:05:20 +0100

    add svg tag in Icon component

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 
-const icons = {};
+const icons = {
+  magnifying_glass: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 520 520"
+      width="30"
+      height="30"
+    >
+      <path
+        fill="none"
+        stroke="#000"
+        strokeWidth="36"
+        strokeLinecap="round"
+        d="M280 278a153 153 0 10-2 2l170 170m-91-117l110 110-26 26-110-110"
+      />
+    </svg>
+  ),
+};
 
-const Icon = ({ name, color }) => (
-  <span className={`icon ${color}`}>{icons[name]}</span>
+const Icon = ({ name, color, className }) => (
+  <span className={`icon ${color} ${className} `}>{icons[name]}</span>
 );
 
 export default Icon;
