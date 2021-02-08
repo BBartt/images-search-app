661fc18e4c5f19aa10bf256a5659deae2d04eac3
Bartosz <bartosz.polakowski@escola.pl>
2021-02-08 15:07:23 +0100

    add autocomplete component to PhotosPage to test if it wirk and make basic view

--- src/pages/PhotosPage/index.js
+++ src/pages/PhotosPage/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import { useParams } from "react-router-dom";
+
+import Autocomplete from "../../components/Autocomplete";
+
 function PhotosPage() {
-  return <div className="photos-page">photos page</div>;
+  const { photoName } = useParams();
+
+  return (
+    <div className="photos-page">
+      <Autocomplete
+        defaultValue={photoName}
+        leftIconName="magnifying_glass"
+        rightIconName="close"
+      />
+      <h1>{photoName}</h1>
+    </div>
+  );
 }
 
 export default PhotosPage;
